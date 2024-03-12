f69141032ff1668ed580991eaebf972f72ecec03
Fool <lazyrrrrererere@gmail.com>
2024-03-12 11:05:05 +0400

    2-1task

--- 2task/2-1task/test.cpp
+++ 2task/2-1task/test.cpp
@@ -1,22 +1,32 @@
-#include <iostream>
+#include<iostream>
+using namespace std;
+
 class Car{
 	public:
-	double x;
-	double v;
-	int p;
-	
+		double x;
+		double v;
+		int p;
+		Car();
 };
-int main(){
-	using namespace std;
-	Car car;
-	cout << "X:" << car.x<< endl;
-	cout << "P:" << car.p<< endl;
-	cout << "V:" << car.v<< endl;
-	car.x = 1;
-	car.p = 11;
-	car.v = 111;
-	cout << "X:" << car.x<< endl;
-	cout << "P:" << car.p<< endl;
-	cout << "V:" << car.v<< endl;
-	return 0;
+
+Car::Car(){
+	x = 0;
+	v = 0;
+	p = 0;
 }
+
+int main(){
+	Car car;
+	cout << "X: " << car.x << endl;
+	cout << "V: " << car.v << endl;
+	cout << "P: " << car.p << endl;
+
+	car.x = 1.5;
+	car.v = 11.5;
+	car.p = 111;
+	
+	cout << "X: " << car.x << endl;
+	cout << "V: " << car.v << endl;
+	cout << "P: " << car.p << endl;
+}
+
