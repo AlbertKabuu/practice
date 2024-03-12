b660205429dbfcaf9c80a3ae6b7b010d5bacca73
Fool <lazyrrrrererere@gmail.com>
2024-03-12 11:41:58 +0400

    2-1task 3

--- 2task/2-1task/test.cpp
+++ 2task/2-1task/test.cpp
@@ -1,32 +1,34 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Car{
+class Car {
+	private:
+		int x, p, v;
+		
 	public:
-		double x;
-		double v;
-		int p;
-		Car();
+		Car() {
+			x = 0;
+			p = 0;
+			v = 0;
+		}
+		Car(int x, int p, int v) {
+			this->x = x;
+			this->p = p;
+			this->v = v;
+		}
+		void showParameters() {
+			cout << "X: " << x << endl;
+			cout << "P: " << p << endl;
+			cout << "V: " << v << endl;
+		}
 };
 
-Car::Car(){
-	x = 0;
-	v = 0;
-	p = 0;
-}
-
-int main(){
-	Car car;
-	cout << "X: " << car.x << endl;
-	cout << "V: " << car.v << endl;
-	cout << "P: " << car.p << endl;
-
-	car.x = 1.5;
-	car.v = 11.5;
-	car.p = 111;
+int main() {
+	Car car(1, 11, 111);
+	car.showParameters();
 	
-	cout << "X: " << car.x << endl;
-	cout << "V: " << car.v << endl;
-	cout << "P: " << car.p << endl;
+	Car car1;
+	car1.showParameters();
 }
+
 
