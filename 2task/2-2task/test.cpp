7db61f59b09db1ae5fd517d1255d96dea28c7e78
Fool <lazyrrrrererere@gmail.com>
2024-03-12 12:42:52 +0400

    2-2task 1(add class Road + Car)

---
+++ 2task/2-2task/test.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+using namespace std;
+
+class Car {
+	private:
+		int x, p, v;
+		
+	public:
+		Car() {
+			x = 0;
+			p = 0;
+			v = 0;
+		}
+		Car(int x, int p, int v) {
+			this->x = x;
+			this->p = p;
+			this->v = v;
+		}
+		void showParameters() {
+			cout << "X: " << x << endl;
+			cout << "P: " << p << endl;
+			cout << "V: " << v << endl;
+		}
+		void setParameters(int x, int p, int v) {
+			this->x = x;
+			this->p = p;
+			this->v = v;
+		}
+		int getX() const {
+			return this->x;
+		}
+		int getP() const {
+			return this->p;
+		}
+		int getV() const {
+			return this->v;
+		}
+};
+
+class Road {
+	private:
+		int length;
+		int width;
+		
+	public:
+		Road() {
+			length = 0;
+			width = 0;
+		}
+		Road(int newLength, int newWidth){
+			length = newLength;
+			width = newWidth;
+		}
+		void setLength(int newLength){
+			length = newLength;
+		}
+		void setWidth(int newWidth){
+			width = newWidth;
+		}
+		void setParameters(){
+			ifstream file;
+			
+			file.open("config.txt");
+			if (file.is_open()) {
+				string line;
+				
+				getline(file, line);
+				size_t slashPos = line.find("\\");
+				string firstNumber = line.substr(0, slashPos);
+				length = stoi(firstNumber);
+
+				getline(file, line);
+				string secondNumber = line.substr(0, slashPos);
+				width = stoi(secondNumber);
+
+				file.close();
+			}
+			else {
+				cout << "Unable to open file" << endl;
+			}
+		}
+		void showParameters() {
+			cout << "Length: " << length << endl;
+			cout << "Width: " << width << endl;
+		}
+		
+};
+
+int main() {
+	Car car(1, 11, 111);
+	car.showParameters();
+	car.setParameters(2, 22, 222);
+	car.showParameters();
+	
+	cout << "x: " << car.getX() << endl;
+	
+	
+	Car car1;
+	car1.showParameters();
+	
+	
+}
+
+
