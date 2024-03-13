d1996dcdd4a244565af3ad4a2301f57d995aae0a
Fool <lazyrrrrererere@gmail.com>
2024-03-13 07:19:02 +0400

    0-3task 1

---
+++ 0task/0-3task/test.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include <fstream>
+using namespace std;
+
+
+class Road
+{
+public:
+	double length;
+	int width;
+	Road(){
+	length = 1.5;
+	width = 11;
+}
+};
+
+int main()
+{
+	Road road;
+	cout << "Length: " << road.length << endl;
+	cout << "Width: " << road.width << endl;
+
+	string line;
+	ifstream fin("config.txt");
+	if (fin.is_open())
+	{
+		getline(fin, line);
+		road.length = stof(line);
+		getline(fin, line);
+		road.width = stoi(line);
+	}
+	fin.close();
+
+
+	cout << "Length: " << road.length << endl;
+	cout << "Width: " << road.width << endl;
+	
+	return 0;
+}
