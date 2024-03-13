52435fa7925701f58e731745ea2d61ca65949810
Fool <lazyrrrrererere@gmail.com>
2024-03-13 07:26:11 +0400

    0-4task 1

---
+++ 0task/0-4task/test.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+class TBook {
+public:
+	string name;
+	string author;
+	string count;
+};
+
+int main() {
+	TBook book1;
+
+	book1.name = "Alices Adventures in Wonderland";
+	book1.author = "Lewis Carroll";
+	book1.count = "1";
+
+	cout << "Name: " << book1.name << endl;
+	cout << "Author: " << book1.author << endl;
+	cout << "Count: " << book1.count << endl;
+
+	return 0;
+}
