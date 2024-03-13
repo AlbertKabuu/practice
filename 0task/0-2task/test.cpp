27d9e03fd4c7c343c59ab084cdbbec178dece1d3
Fool <lazyrrrrererere@gmail.com>
2024-03-13 07:09:48 +0400

    0-2task 1

---
+++ 0task/0-2task/test.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+
+class Road
+{
+public:
+	double length;
+	int width;
+	Road(){
+	length = 1.5;
+		width = 11;
+}
+};
+
+int main()
+{
+	Road road;
+	cout << "Length: " << road.length << endl;
+	cout << "Width: " << road.width << endl;
+
+	cout << "enter length: " << endl;
+	cin >> road.length;
+	cout << "enter width: " << endl;
+	cin >> road.width;
+
+	cout << "Length: " << road.length << endl;
+	cout << "Width: " << road.width << endl;
+	
+	return 0;
+}
