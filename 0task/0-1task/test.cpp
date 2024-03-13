81c5cdb745dff233b724309c0170fcfec72e79ce
Fool <lazyrrrrererere@gmail.com>
2024-03-13 07:01:09 +0400

    0-1task 1

---
+++ 0task/0-1task/test.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<iostream>
+class Road {
+public:
+	double lenght;
+	int Width;
+};
+int main() {
+	Road road;
+	road.lenght = 11;
+	road.Width = 1;
+	std::cout << "lenght" << road.lenght << std::endl;
+	std::cout << "Width:"<< road.Width << std::endl;
+	return(0);
+}
